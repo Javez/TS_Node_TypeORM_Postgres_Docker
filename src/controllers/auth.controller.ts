61483b402d1c387865aa0c3a59f7d586d81c01ea
Oleksii <do.gen.enterprise@gmail.com>
2025-02-10 12:29:42 +0200

    setup auth controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,4 +1,4 @@
-import { NextFunction, Router } from 'express';
+import * as express from 'express';
 import CreateUserDTO from '../dto/user.dto';
 import validationMiddleware from '../middleware/validation.middleware';
 import Controller from '../interfaces/controller.interface';
@@ -6,16 +6,35 @@ import AuthService from '../service/auth/auth.service';
 
 class AuthController implements Controller {
     public path = '/auth';
-    public router = Router();
+    public router = express.Router();
     public authService = new AuthService();
 
     constructor() {
         this.initializeRoutes();
     }
 
-    private initializeRoutes() {}
+    private initializeRoutes() {
+        this.router.post(
+            `${this.path}/register`,
+            validationMiddleware(CreateUserDTO),
+            this.registration
+        );
+    }
 
-    private registration(req: Request, res: Response, next: NextFunction) {}
+    private registration = async (
+        request: express.Request,
+        response: express.Response,
+        next: express.NextFunction
+    ) => {
+        const userData: CreateUserDTO = request.body;
+        try {
+            const { cookie, user } = await this.authService.register(userData);
+            response.setHeader('Set-Cookie', [cookie]);
+            response.send(user);
+        } catch (error) {
+            next(error);
+        }
+    };
 }
 
 export default AuthController;
